4e644ef35cf8bf9150211a217afea7e42173b9aa
Divija11026 <131694752+Divija11026@users.noreply.github.com>
2023-05-18 18:44:06 +0530

    Add files via upload

---
+++ audio.py
@@ -0,0 +1,19 @@
+import os
+import random
+from playsound import playsound
+
+folder_path = '/home/divija/playlist'  
+
+# List all files in the folder
+files = os.listdir(folder_path)
+
+# Randomize the file list
+random.shuffle(files)
+
+# Iterate over the randomized files
+for file_name in files:
+    file_path = os.path.join(folder_path, file_name)
+    absolute_path = os.path.abspath(file_path)
+    print(absolute_path)
+    # Play the sound file
+    playsound(absolute_path)
